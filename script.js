daa32a9f1b8c42c90eb9b8641d8b927ef689bff7
Alex Parchment <alexparchment@mac.home>
2024-12-27 16:59:02 +0000

    DOM Element Selectors for Game Interface.

---
+++ script.js
@@ -0,0 +1,7 @@
+const header = document.querySelector(".header");
+const roundResult = document.querySelector(".round-result");
+
+const choicesContainer = document.querySelector(".choices-container");
+const playerStage = document.querySelector (".player-choice.stage");
+const playerScoreContainer = document.querySelector(".player-score");
+
